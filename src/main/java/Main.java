03de89b2977c588095d3370a724331ae328e192d
austin-weibin <weibin.sun1@yahoo.com>
2022-08-01 21:53:44 -0400

    main

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+/**
+ * @author Weibin Sun
+ * @create 8/1/22 12:28 AM
+ */
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "ab4562c";
+        System.out.println();
+        String s2 = "ab4562c";
+        System.out.println(s2.hashCode());
+
+        String s3 = new String("ab4562c");
+        System.out.println(s3.hashCode());
+
+        System.out.println(s1 == s2);
+        System.out.println(s2 == s3);
+        System.out.println(s2.equals(s3));
+
+    }
+
+
+}
