3409b4c80c5668a80893dfdcd4679097de9c7e38
sunweibin <weibin.sun1@yahoo.com>
2022-08-04 21:21:04 -0400

    AntraOA

---
+++ src/main/java/AntraOA/Box.java
@@ -0,0 +1,38 @@
+package AntraOA;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Box {
+    public static void main(String[] args) {
+        List<List<Integer>> queries = Arrays.asList(Arrays.asList(2,1), Arrays.asList(2,3));
+        List<Long> longs = numberOfWays(queries);
+        for(Long l : longs) {
+            System.out.println(l);
+        }
+    }
+
+    public static List<Long> numberOfWays(List<List<Integer>> queries) {
+        List<Long> res = new ArrayList<>();
+        for(List<Integer> query: queries)  {
+            Long total = 0l;
+            Integer row = query.get(0);
+            Integer column = query.get(1);
+            total = row > column ? getTotal(row, column) : getTotal(column, row);
+            res.add(total);
+        }
+        return res;
+    }
+
+    private static Long getTotal(Integer big, Integer small) {
+        Long res = 0l;
+        while(small > 0) {
+            res += big * small;
+            big--;
+            small--;
+        }
+        return res;
+    }
+}
+
---
+++ src/main/java/AntraOA/Properties.java
@@ -0,0 +1,17 @@
+package AntraOA;
+
+import java.util.PriorityQueue;
+
+public class Properties {
+    public static void main(String[] args) {
+        PriorityQueue toDo = new PriorityQueue();
+        toDo.add("dishes");
+        toDo.add("laundry");
+        toDo.add("bills");
+        toDo.offer("bills");
+        System.out.print(toDo.size() + " " + toDo.poll());
+        System.out.print(" " + toDo.peek() + " " + toDo.poll());
+        System.out.println(" " + toDo.poll() + " " + toDo.poll());
+        // 4 bills bills bills dishes laundry
+    }
+}
